b9fb1f50076ced37ec108511231b976b3089a987
TechyWecky69 <techyisontop@gmail.com>
2026-08-17 22:18:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Set this before deploying.
 window.DASHBOARD_CONFIG = {
-  apiBase: "https://51.155.195.251:8080",
-  applicationKey: "kdrnnnfbHE6AWwVGixp38qIV_GSgMxbvCXn3mYd1L4E"
+  apiBase: "",
+  applicationKey: ""
 };
